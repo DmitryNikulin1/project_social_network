2d13d228fdd4b278f9985dd8e6542bc35ed476e5
DmitryNikulin1 <Saimond1534@gmail.com>
2023-11-23 22:12:29 +0700

    add color

--- src/Components/UI/MyInput/MyInput.styled.ts
+++ src/Components/UI/MyInput/MyInput.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MyInp = styled.input`
   width: 160px;
   height: 20px;
-  background-color: #6287e9;
+  color: #6287e9;
   border-radius: 5px;
   border: none;
 `;
